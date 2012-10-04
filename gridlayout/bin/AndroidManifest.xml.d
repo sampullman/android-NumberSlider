a8d00a140c31f93b6386a0f5d6d86b523563a91b
Sam Pullman <sam@comp2.(none)>
2012-10-03 17:17:23 -0700

    added .gitignore and untracked files

--- gridlayout/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/sam/Desktop/git/android/numberSliderGame/gridlayout/bin/AndroidManifest.xml : \
-/home/sam/Desktop/git/android/numberSliderGame/gridlayout/AndroidManifest.xml \
--- gridlayout/gen/R.java.d
+++
@@ -1,4 +0,0 @@
-/home/sam/Desktop/git/android/numberSliderGame/gridlayout/gen/android/support/v7/gridlayout/R.java \
- : /home/sam/Desktop/git/android/numberSliderGame/gridlayout/res/values/dimens.xml \
-/home/sam/Desktop/git/android/numberSliderGame/gridlayout/res/values/attrs.xml \
-/home/sam/Desktop/git/android/numberSliderGame/gridlayout/bin/AndroidManifest.xml \
